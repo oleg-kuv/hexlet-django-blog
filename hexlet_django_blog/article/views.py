e27368c7f08e2b2711a9348e21e1a61c78a4dc99
Oleg Kuvshinov <oleg.kuvshinov@piklema.com>
2023-09-25 14:50:51 +0300

    lesson 4

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'index.html', context={
+        'templ': 'Article template',
+    })
